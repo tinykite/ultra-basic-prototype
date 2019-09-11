85399720de02699c80d191bd7eb28782d1da3216
tinykite <dakota@tinykitelab.com>
2019-09-11 07:45:13 -0500

    feat: add brew methods section

---
+++ src/components/Section6/Section6.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Section6.scss';
+
+const Section6 = () => {
+	return(
+		<>
+		<div class="section6">
+		<h2 class="section6__sectionHeading">Brew Methods</h2>
+		</div>
+		</>
+	)
+}
+
+export default Section6;
