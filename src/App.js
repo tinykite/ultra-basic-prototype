2588781f18712e466c9fae271b18c5afcec5e908
tinykite <dakota@tinykitelab.com>
2019-10-15 08:40:35 -0500

    refactor: remove outdated files

--- src/App.js
+++ src/App.js
@@ -3,28 +3,17 @@ import './index.scss';
 import Hero from './components/Hero/Hero';
 import Introduction from './components/Introduction/Introduction';
 import Section1 from './components/Section1/Section1';
-import Section2 from './components/Section2/Section2';
-import Section3 from './components/Section3/Section3';
-import Section4 from './components/Section4/Section4';
-import Section6 from './components/Section6/Section6';
+import BasicAdvice from './components/BasicAdvice/BasicAdvice';
 import Aeropress from './components/Aeropress/Aeropress';
 import KalitaWave from './components/KalitaWave/KalitaWave';
 import FrenchPress from './components/FrenchPress/FrenchPress';
 
 function App() {
 	return (
-		<div className="App">
+		<>
 			<Hero />
 			<Introduction />
-			<Section1 />
-			<Section2 />
-			<Section3 />
-			<Section4 />
-			<Section6 />
-			<Aeropress />
-			<KalitaWave />
-			<FrenchPress />
-		</div>
+		</>
 	)
 }
 
--- src/components/KalitaWave/KalitaWave.jsx
+++ src/components/KalitaWave/KalitaWave.jsx
@@ -14,38 +14,46 @@ const KalitaWave = () => {
 				<h2 className="KalitaWave__heading">Kalita Wave</h2>
 				<p className="KalitaWave__text">One thing many individuals do wax poetic about is the importance of good gear. In many ways, fundamental coffee tools — and the somewhat ritualistic process they are used in — are just as important as unspoiled coffee beans.</p>
 			</div>
-      <svg className="KalitaWave__illustration" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 313 365" height="365" width="313">
-      <g
-        fill="none"
-        fillRule="evenodd"
-        stroke="none"
-        strokeWidth="1"
-        transform="translate(-879 -6699)"
-      >
+      <svg className="KalitaWave__illustration"
+      xmlns="http://www.w3.org/2000/svg"
+      width="301"
+      height="351"
+      viewBox="0 0 301 351"
+    >
+      <g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
         <path
-          stroke="#19224F"
-          strokeWidth="11"
-          d="M1136.305 6727l-57.862 102h46.252l57.862-102h-46.252z"
+          fill="#FDF1F2"
+          fillRule="nonzero"
+          d="M0.05 0.74H300.05V350.74H0.05z"
+          transform="translate(-849 -5703) translate(849 5703)"
         />
-        <path fill="#19224F" d="M903 6855.5H1130V6866.5H903z" />
-        <path
-          fill="#19224F"
-          d="M1054.40755 6699.5L1149 6860.5 879 6860.5 970.573 6699.5z"
-          transform="rotate(-180 1014 6780)"
-        />
-        <path
-          stroke="#19224F"
-          strokeWidth="11"
-          d="M945.5 6942v82.5c0 17.95 14.55 32.5 32.5 32.5h69c17.95 0 32.5-14.55 32.5-32.5V6942h-134z"
-        />
-        <path
-          fill="#19224F"
-          stroke="#19224F"
-          strokeWidth="11"
-          d="M946.595 7028c1.276 16.781 15.297 30 32.405 30h68c17.108 0 31.129-13.219 32.405-30h-132.81z"
-        />
-        <path className="u-rounded" fill="#19224F" d="M1016 6860.5H1027V6921.5H1016z" stroke-linecap="round" />
-        <path className="u-rounded" fill="#19224F" d="M1031 6836.5H1042V6897.5H1031z" stroke-linecap="round" />
+        <g transform="translate(-849 -5703) translate(849 5703) translate(8 2)">
+          <path
+            stroke="#1B234F"
+            strokeWidth="11"
+            d="M76 232.42v78.82a31 31 0 0031.05 31H173a31 31 0 0031.05-31v-78.82H76z"
+          />
+          <path
+            fill="#1B234F"
+            stroke="#1B234F"
+            strokeWidth="11"
+            d="M77.09 314.58c1.25 16.19 14.762 28.682 31 28.66h65c16.236.017 29.745-12.473 31-28.66h-127z"
+          />
+          <path
+            fill="#8D86A0"
+            d="M139.35 128.79c2.76 0 5 3.36 5 7.5V197c0 3.74-2.24 6.76-5 6.76s-5-3-5-6.76v-60.71c0-4.14 2.24-7.5 5-7.5zM153.71 129.19a5 5 0 015 5v40.49a4.78 4.78 0 01-5 4.51 4.77 4.77 0 01-5-4.51v-40.49a5 5 0 015-5z"
+            opacity="0.5"
+          />
+          <path
+            fill="#1B234F"
+            fillRule="nonzero"
+            d="M55.79 149.99H205.79V159.99H55.79z"
+          />
+          <path
+            fill="#1B234F"
+            d="M240.42 20.3L251.93.05H.38l88.13 150h78.11l13.64-24h43.37l60-105.77-43.21.02zm-23 95h-30.79L234.46 31h30.76l-47.8 84.3z"
+          />
+        </g>
       </g>
     </svg>
       <div className="KalitaWave__waveBox">
