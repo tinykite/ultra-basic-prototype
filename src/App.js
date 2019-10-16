34163d22fdbb6a8832d1e46b4b6443e49354a8d3
tinykite <dakota@tinykitelab.com>
2019-10-16 14:17:11 -0500

    Feat: Add Tools Interstitial

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './index.scss';
 import Hero from './components/Hero/Hero';
 import Introduction from './components/Introduction/Introduction';
 import Interstitial from './components/Interstitial/Interstitial';
+import Tip from './components/Tip/Tip';
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<Introduction />
 			<Interstitial Title={"Ultra Basic Advice"} Description={"Because everyone needs to start somewhere. These are the tips I wish I always had."}/>
 			<Tip />
+			<Interstitial Title={"Ultra Basic Tools"} Description={"Basic but beautiful methods of making coffee by hand."}/>
 		</>
 	)
 }
