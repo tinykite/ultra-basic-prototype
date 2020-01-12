2f050c137a2b60a6e901910485dc339b7fa4d01e
tinykite <dakota@tinykitelab.com>
2020-01-12 14:07:31 -0600

    Fix: Illustration cropping and overlap issues on mobile

--- src/Introduction.jsx
+++ src/Introduction.jsx
@@ -6,22 +6,24 @@ import styled from 'styled-components';
 const WaveBottom = styled(motion.svg)`
   width: 100%;
   height: auto;
-  position: absolute;
-  bottom: 0;
   background: #fff;
+  @media (min-width: 500px) {
+    position: absolute;
+    bottom: 0;
+  }
 `;
 
 const ScrollContainer = styled(motion.div)`
   background: black;
   position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  overflow: hidden;
-  margin: 0 auto;
 
-  @media (max-width: 720px) {
-    padding: 5vh 0;
+  @media (min-width: 500px) {
+    margin: 0 auto;
+    justify-content: center;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+    height: 85vh;
   }
 
   @media (min-width: 720px) {
@@ -29,31 +31,42 @@ const ScrollContainer = styled(motion.div)`
   }
 `;
 
-const Illustration = styled(motion.svg)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 25px;
-  width: 52px;
+const IllustrationItem = styled.svg`
+  @media (max-width: 500px) {
+    width: 65px;
+    height: auto;
+  }
 
-  @media (min-width: 720px) {
-    height: 50px;
-    width: auto;
+  @media (min-width: 500px) {
+    position: absolute;
+    top: 0;
   }
 `;
 
 const Story = styled(motion.article)`
-  width: 80%;
+  width: 70%;
   max-width: 550px;
   position: relative;
-  padding-top: 50px;
+  padding: 5vh 0;
+  margin-left: 2rem;
+
+  @media (min-width: 500px) {
+    padding-top: 50px;
+    margin: 0;
+  }
 
   @media (min-width: 720px) {
-    padding-top: 100px;
+    width: 80%;
+    padding-top: 85px; /* Space for illustrated hash marks */
   }
 `;
 
 const StoryItem = styled(motion.p)`
+  :nth-of-type(1) {
+    @media (max-width: 720px) {
+      padding-top: 1rem;
+    }
+  }
   color: #ffffff;
   font-size: 14px;
 
@@ -69,13 +82,21 @@ const StoryItem = styled(motion.p)`
 const Drips = styled(motion.svg)`
   position: absolute;
   top: 10px;
-  right: 7%;
+  right: 2%;
   width: 40px;
   height: auto;
 
+  @media (min-width: 500px) {
+    right: 5%;
+  }
+
   @media (min-width: 720px) {
-    right: 20%;
+    right: 8%;
     width: 52px;
+  }
+
+  @media (min-width: 900px) {
+    right: 20%;
   }
 `;
 
@@ -95,29 +116,23 @@ const Introduction = () => {
     <>
       <ScrollContainer ref={ref}>
         <Story>
-          <Illustration
+          <IllustrationItem
+            width="104"
+            height="44"
+            viewBox="0 0 104 44"
             xmlns="http://www.w3.org/2000/svg"
-            viewport="0 0 44 104"
-            fill="none"
           >
-            <g
-              fill="#fff"
-              fillRule="evenodd"
-              clip-path="url(#clip0)"
-              clipRule="evenodd"
-            >
-              <path d="M19.401 14.495L32.358 1.131C33.232.23 34.954.01 36.202.643c1.096.555 1.483 1.6.918 2.477l-9.528 14.795c-2.472 3.838-5.405 7.511-8.766 10.978L5.87 42.257c-.875.903-2.597 1.122-3.845.49-1.096-.556-1.483-1.6-.918-2.478l9.528-14.795c2.472-3.838 5.406-7.512 8.766-10.979z"></path>
-              <path d="M18.826 14.495L5.87 1.131C4.995.23 3.273.01 2.025.643c-1.096.555-1.483 1.6-.918 2.477l9.528 14.795c2.472 3.838 5.406 7.511 8.766 10.978l12.956 13.364c.875.903 2.597 1.122 3.845.49 1.096-.556 1.483-1.6.918-2.478l-9.528-14.795c-2.472-3.838-5.405-7.512-8.766-10.979zm66.575 0L98.358 1.131c.875-.902 2.597-1.121 3.845-.488 1.096.555 1.483 1.6.918 2.477l-9.528 14.795c-2.472 3.838-5.406 7.511-8.766 10.978L71.87 42.257c-.875.903-2.597 1.122-3.845.49-1.096-.556-1.483-1.6-.918-2.478l9.528-14.795c2.472-3.838 5.406-7.512 8.766-10.979z"></path>
-              <path d="M84.826 14.495L71.87 1.131C70.995.23 69.273.01 68.025.643c-1.096.555-1.483 1.6-.918 2.477l9.528 14.795c2.472 3.838 5.406 7.511 8.766 10.978l12.956 13.364c.875.903 2.597 1.122 3.845.49 1.096-.556 1.483-1.6.918-2.478l-9.528-14.795c-2.472-3.838-5.406-7.512-8.766-10.979z"></path>
-              <path d="M52.401 14.495L65.358 1.131C66.232.23 67.954.01 69.202.643c1.096.555 1.483 1.6.918 2.477l-9.528 14.795c-2.472 3.838-5.405 7.511-8.767 10.978L38.87 42.257c-.875.903-2.597 1.122-3.845.49-1.096-.556-1.483-1.6-.918-2.478l9.528-14.795c2.472-3.838 5.406-7.512 8.767-10.979z"></path>
-              <path d="M51.826 14.495c3.36 3.467 6.294 7.141 8.766 10.98l9.528 14.794c.565.878.178 1.922-.918 2.477-1.248.633-2.97.414-3.845-.489L52.401 28.893c-3.36-3.467-6.294-7.14-8.766-10.978L34.107 3.12c-.565-.878-.178-1.922.918-2.477 1.248-.633 2.97-.414 3.845.488l12.956 13.365z"></path>
+            <g fill="#FFF" fillRule="evenodd">
+              <path d="M19.402 14.495L32.357 1.131C33.232.23 34.954.01 36.202.642c1.096.556 1.483 1.6.918 2.478l-9.528 14.795c-2.472 3.838-5.406 7.511-8.766 10.978L5.87 42.257c-.875.903-2.597 1.122-3.845.49-1.096-.556-1.483-1.6-.918-2.478l9.528-14.795c2.472-3.838 5.406-7.512 8.767-10.979z" />
+              <path d="M18.826 14.495L5.87 1.131C4.995.23 3.273.01 2.025.642c-1.096.556-1.483 1.6-.918 2.478l9.528 14.795c2.472 3.838 5.406 7.511 8.767 10.978l12.955 13.364c.875.903 2.597 1.122 3.845.49 1.096-.556 1.483-1.6.918-2.478l-9.528-14.795c-2.472-3.838-5.406-7.512-8.766-10.979z" />
+              <path d="M52.402 14.495L65.357 1.131C66.232.23 67.954.01 69.202.642c1.096.556 1.483 1.6.918 2.478l-9.528 14.795c-2.472 3.838-5.406 7.511-8.766 10.978L38.87 42.257c-.875.903-2.597 1.122-3.845.49-1.096-.556-1.483-1.6-.918-2.478l9.528-14.795c2.472-3.838 5.406-7.512 8.767-10.979z" />
+              <path d="M51.826 14.495L38.87 1.131C37.995.23 36.273.01 35.025.642c-1.096.556-1.483 1.6-.918 2.478l9.528 14.795c2.472 3.838 5.406 7.511 8.767 10.978l12.955 13.364c.875.903 2.597 1.122 3.845.49 1.096-.556 1.483-1.6.918-2.478l-9.528-14.795c-2.472-3.838-5.406-7.512-8.766-10.979z" />
+              <g>
+                <path d="M85.402 14.495L98.357 1.131c.875-.902 2.597-1.121 3.845-.489 1.096.556 1.483 1.6.918 2.478l-9.528 14.795c-2.472 3.838-5.406 7.511-8.766 10.978L71.87 42.257c-.875.903-2.597 1.122-3.845.49-1.096-.556-1.483-1.6-.918-2.478l9.528-14.795c2.472-3.838 5.406-7.512 8.767-10.979z" />
+                <path d="M84.826 14.495c3.36 3.467 6.294 7.141 8.766 10.98l9.528 14.794c.565.877.178 1.922-.918 2.477-1.248.633-2.97.414-3.845-.489L85.402 28.893c-3.361-3.467-6.295-7.14-8.767-10.978L67.107 3.12c-.565-.878-.178-1.922.918-2.478 1.248-.632 2.97-.413 3.845.49l12.956 13.363z" />
+              </g>
             </g>
-            <defs>
-              <clipPath id="clip0">
-                <path fill="#fff" d="M0 0h104v44H0z"></path>
-              </clipPath>
-            </defs>
-          </Illustration>
+          </IllustrationItem>
           <StoryItem>
             At a punk house in Chicago I lived in during college,
             coffee was a giant pot of water with an industrial dose of
