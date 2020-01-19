b003645eb9a4a5571f93dd2b30532df4e143d7cd
tinykite <dakota@tinykitelab.com>
2020-01-19 08:46:51 -0600

    Remove splitting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Splitting from 'splitting';
-
-Splitting();
 
 ReactDOM.render(<App />, document.getElementById('root'));
